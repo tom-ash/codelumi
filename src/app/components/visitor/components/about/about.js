79799e1703539120deb8f52b00a07df643e379d1
tom-ash <tomasz.bogusz@outlook.com>
2019-09-01 11:45:09 +0200

    ADD about

--- src/app/components/visitor/components/about/about.js
+++ src/app/components/visitor/components/about/about.js
@@ -3,16 +3,43 @@ import { connect } from 'react-redux'
 import { mapStateToProps, mapDispatchToProps } from './constants/mappers'
 import { changeRoute } from '../../../../functions/routers'
 import './styles/styles.scss'
+import { pageHeaderProvider } from '../../../../functions/providers/headers'
+import { languageHandler, languageObjectHandler } from '../../../../functions/language-handler'
+import { features } from './constants/features'
 
 class About extends React.Component {
   constructor(props) {
     super(props)
     this.changeRoute = changeRoute.bind(this)
+    this.pageHeaderProvider = pageHeaderProvider.bind(this)
+    this.languageHandler = languageHandler.bind(this)
+    this.languageObjectHandler = languageObjectHandler.bind(this)
   }
   render() {
     return(
       <div id='visitor-about'>
-        --- O SERWISIE ---
+        {this.pageHeaderProvider('fas fa-info-circle', { polish: 'O Serwisie', english: 'About' })}
+        <div className='paragraph'>
+          {
+          this.languageHandler(
+          <span>Serwis internetowy znajdujący się po adresem <u>warsawlease.pl</u> umożliwia wyszukiwanie biur oraz&nbsp;lokali użytkowych znajdujących się w obszarze m.&nbsp;st.&nbsp;Warszawy.</span>,
+          <span>The Internet Service located under the address <u>warsawlease.pl</u> alllows searching of offices and commercial premises located within the area of&nbsp;the&nbsp;Capital City of Warsaw.</span>)
+          }
+        </div>
+        <div className='paragraph features'>
+          {
+          this.languageHandler(
+          <span>Serwis jest:</span>,
+          <span>The Service is:</span>)
+          }
+        </div>
+        {
+        features.map((feature, index) => (
+          <div className='feature' key={index}>
+            <i className="fas fa-check-circle"></i>  {this.languageObjectHandler(feature)}
+          </div>
+        ))
+        }
       </div>
     )
   }
---
+++ src/app/components/visitor/components/about/constants/features.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const features = [
+  {
+    polish: <span>Mobilny - Serwis działa zgodnie z zasadą <i>Mobile First</i>, czyli stara się prezentować treści w swej wersji mobilnej w czytelny sposób oraz przy zachowaniu pełni funkcjonalności wersji desktopowej.</span>,
+    english: <span>Mobile - the Service operates according to the principle of <i>Mobile First</i>, therefore in its mobile version the Service aims at presenting content in a clear manner and with retaining full desktop version functionality.</span>
+  },
+  {
+    polish: <span>Lokalny - Serwis przeznaczony jest dla zamieszczania ogłoszeń wyłącznie z&nbsp;obszaru m.&nbsp;st.&nbsp;Warszawy.</span>,
+    english: <span>Local - the Service is dedicated to publishing announcements exclusively from the area of the Capital City of Warsaw.</span>
+  },
+  {
+    polish: <span>Wyspecjalizowany - Serwis przeznaczony jest dla zamieszczania ogłoszeń dotyczącyh wyłącznie nieruchomości komercyjnych: biur oraz lokali użytkowych.</span>,
+    english: <span>Specialized - the Service is dedicated to publishing announcements regarding exclusively commercial real estates: offices and commercial premises.</span>
+  },
+  {
+    polish: <span>Dwujęzyczny - ogłoszenia dodawane są automatycznie w językach polskim oraz angielskim.</span>,
+    english: <span>Bilingual - the announcements are automatically added in Polish and&nbsp;English languages.</span>
+  },
+  {
+    polish: <span>Ergonomiczny - Serwis umożliwia wyszukiwania ogłoszeń na&nbsp;liście lub&nbsp;mapie.</span>,
+    english: <span>Ergonomic - the Service allows searching of announcement on a list or&nbsp;on&nbsp;a&nbsp;map.</span>
+  },
+]
