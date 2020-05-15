c8dc2e1b53fa451a7186c089d72f45ef41dc8a57
tom-ash <tomasz.bogusz@outlook.com>
2020-05-15 18:47:07 +0200

    CHG index routes descriptions

--- src/shared/routes/core-index-routes.js
+++ src/shared/routes/core-index-routes.js
@@ -1,22 +1,17 @@
-const genericDescription = {
-  pl: 'Masz nieruchomość na wynajem w Warszawie? Na warsawlease.pl w łatwy sposób dodasz bezpłatne ogłoszenie wynajmu mieszkania, lokalu użytkowego lub biura. Dodane ogłoszenia prezentowane są na mapie oraz na liście. Mapa ogłoszeń umożliwia precyzyjne wyszukiwanie w oparciu o wybranę lokalizację, a lista szybkie przeglądanie zdjęć.',
-  en: 'Do you have a real estate to lease in Warsaw? On warsawlease.pl you can easily add a free announcement of lease of an apartment, usable premises and/or and office. The added announcements are presented on the map and on the list. The announcement map allows for precise search based on the selected location and the list allows for quick picture browsing.'
-}
-
 export const coreIndexRoutes = {
   showAnnouncementIndexMap: {
     sender: 'map',
     pl: {
       regEx: /^\/?$/,
       url: '/',
-      title: 'Mapa nieruchomości na wynajem, Warszawa - Mieszkania, lokale użytkowe i biura do wynajęcia w Warszawie',
-      description: genericDescription.pl
+      title: 'Ogłoszenia nieruchomości na wynajem - Warszawa - Mapa - Mieszkania, biura i lokale użytkowe',
+      description: 'Bezpłatne ogłoszenia nieruchomości na wynajem w Warszawie na mapie. Przeglądaj i dodawaj darmowe ogłoszenia wynajmu w kategorii: mieszkanie, biuro, lokal użytkowy.'
     },
     en: {
       regEx: /^\/?$/,
       url: '/',
-      title: 'Map of Real Estates for Lease, Warsaw - Apartments, Usable Premises and Offices for Lease in Warsaw',
-      description: genericDescription.en
+      title: 'Announcements of Real Estate Lease - Warsaw - Map - Apartments, Offices and Usable premises',
+      description: 'Free announcements of real estate lease in Warsaw on the map. Search and add free announcements of lease in the categories: apartment, office, usable premises.'
     },
     overshadow: false
   },
@@ -25,14 +20,14 @@ export const coreIndexRoutes = {
     pl: {
       regEx: /^\/?lista\/?$/,
       url: '/lista',
-      title: 'Lista nieruchomości na wynajem, Warszawa - Mieszkania, lokale użytkowe i biura do wynajęcia w Warszawie',
-      description: genericDescription.pl
+      title: 'Ogłoszenia nieruchomości na wynajem - Warszawa - Lista - Mieszkania, biura i lokale użytkowe',
+      description: 'Bezpłatne ogłoszenia nieruchomości na wynajem w Warszawie na liście. Przeglądaj i dodawaj darmowe ogłoszenia wynajmu w kategorii: mieszkanie, biuro, lokal użytkowy.'
     },
     en: {
       regEx: /^\/?list\/?$/,
       url: '/list',
-      title: 'List of Real Estates for Lease, Warsaw - Apartments, Usable Premises and Offices for Lease in Warsaw',
-      description: genericDescription.en
+      title: 'Announcements of Real Estate Lease - Warsaw - List - Apartments, offices and usable premises',
+      description: 'Free announcements of real estate lease in Warsaw on the list. Search and add free announcements of lease in the categories: apartment, office, usable premises.'
     },
     needsAnnouncementIndexMap: true,
     overshadow: true
@@ -48,14 +43,14 @@ export const coreIndexRoutes = {
     pl: {
       regEx: /^\/?dodaj-ogloszenie\/?$/,
       url: '/dodaj-ogloszenie',
-      title: 'Dodaj Bezpłatne Ogłoszenie Wynajmu Nieruchomości w Warszawie',
-      description: genericDescription.pl
+      title: 'Dodaj bezpłatne ogłoszenie wynajmu nieruchomości w Warszawie w kategorii: mieszkanie, lokal użytkowy, biuro',
+      description: 'Dodaj darmowe ogłoszenie wynajmu nieruchomości w Warszawie w kategorii: mieszkanie, lokal użytkowy lub biuro.'
     },
     en: {
       regEx: /^\/?add-announcement\/?$/,
       url: '/add-announcement',
-      title: 'Add Free Announcement of Real Estate Lease in Warsaw',
-      description: genericDescription.en
+      title: 'Add Free Announcement of Lease of Real Estate in Warsaw in the category: Apartment, Usable premises, Office',
+      description: 'Add free announcement of lease of real estate in Warsaw in the category: Apartment, Usable premises, Office.'
     },
     overshadow: false
   }
