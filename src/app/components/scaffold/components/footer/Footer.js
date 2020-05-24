39b5b68878bb1cc6761062be68dfad1b497e5fe5
tom-ash <tomasz.bogusz@outlook.com>
2020-05-24 12:44:07 +0200

    CHG footer styles

--- src/app/components/scaffold/components/footer/Footer.js
+++ src/app/components/scaffold/components/footer/Footer.js
@@ -24,20 +24,14 @@ class Footer extends React.Component {
       <div id='footer'>
         <div className='inner'>
           <div className='links'>
-            <div className='column'>
-              <ManagedLink {...this.aboutManger()} />
               <ManagedLink {...this.contactManager()} />
               <ManagedLink {...this.termsOfServiceManager()} />
-            </div>
-            <div className='column'>
               <ManagedLink {...this.cookiesPolicyManager()} />
               <ManagedLink {...this.privacyPolicyManager()} />
               <ManagedLink {...this.privacySettingsManager()} />
-            </div>
-            <div className='float-clear' />
           </div>
           <div className='copyright'>
-            <i className='far fa-copyright' /> warsawlease.pl 2020
+            <i className='far fa-copyright' /> Warsaw Digital Sp. z o.o. 2020 {this.languageObjectHandler({ pl: 'Wszelkie prawa zastrzeżone.', en: 'All rights reserved.' })}
           </div>
           <div className='float-clear' />
         </div>
