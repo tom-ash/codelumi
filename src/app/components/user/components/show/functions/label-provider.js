ccf809912d969db874e54cf9b63096c8e4a41891
tom-ash <tomasz.bogusz@outlook.com>
2020-06-20 15:28:51 +0200

    CHG account links style

--- src/app/components/user/components/show/functions/label-provider.js
+++ src/app/components/user/components/show/functions/label-provider.js
@@ -3,7 +3,6 @@ import React from 'react'
 export function labelProvider(icon, pl, en) {
   return (
     <span>
-      <i className={icon}></i>
       <span className='text'>{this.languageHandler(pl, en)}</span>
     </span>
   )
