14388d3dbfe11b7c32f3f049c38d1766af458e0d
tom-ash <tomasz.bogusz@outlook.com>
2020-09-25 07:50:03 +0200

    FIX facebook sharer

--- src/app/components/announcement/functions/share-on-facebook.js
+++ src/app/components/announcement/functions/share-on-facebook.js
@@ -9,12 +9,12 @@ export function shareOnFacebook(url, isMobile) {
   if (isMobile) return window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`)
 
   window.open(
-    'http://www.facebook.com/sharer.php?s=100&p[title]=' +
-    '&p[url]=' + url,
+    'http://www.facebook.com/sharer.php?' +
+    '&u=' + url,
     'sharer',
     'top=' + winTop +
     ',left=' + winLeft +
-    ',toolbar=0,status=0,width=' + winWidth +
+    ',width=' + winWidth +
     ',height=' + winHeight
   )
 }
