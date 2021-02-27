815dd39da5a038657861f297e4c3955f941bacdf
tom-ash <tomasz.bogusz@outlook.com>
2021-02-27 14:00:32 +0100

    CHG APP_ENV

--- src/sites/warsawlease-pl/app/app.js
+++ src/sites/warsawlease-pl/app/app.js
@@ -39,6 +39,7 @@ class App extends React.Component {
 
     return (
       <AppContext.Provider value={AppContextValue}>
+        {console.log(APP_ENV)}
         <div id='app-container'>
           <Header />
           <div id='app-inner-container'>
