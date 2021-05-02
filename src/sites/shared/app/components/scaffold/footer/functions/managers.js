3a725a5d5d678baeb1ba6a3b72b76a7686b6ae2c
tom-ash <tomasz.bogusz@outlook.com>
2021-05-02 10:58:35 +0200

    CHG refactored managers

--- src/sites/shared/app/components/scaffold/footer/functions/managers.js
+++ src/sites/shared/app/components/scaffold/footer/functions/managers.js
@@ -1,71 +1,26 @@
-export function contactManager(props) {
-  const { langs, urls, titles, clientUrl, changeRoute, langHandler } = props
-  const classNames = { container: 'link' }
-  const hrefLang = langHandler(langs)
-  const href = `${clientUrl}/${langHandler(urls.VISITOR_CONTACT_URLS)}`
-  const title = langHandler(titles.VISITOR_CONTACT_TITLES)
-  const label = title
-  const onClick = () => changeRoute({ href })
-  
-  return { classNames, href, hrefLang, title, label, onClick }
-}
+const links_data = [
+  { name: 'contactManager', urls_key: 'VISITOR_CONTACT_URLS', titles_key: 'VISITOR_CONTACT_TITLES' },
+  { name: 'termsOfServiceManager', urls_key: 'VISITOR_TERMS_OF_SERVICE_URLS', titles_key: 'VISITOR_TERMS_OF_SERVICE_TITLES' },
+  { name: 'cookiesPolicyManager', urls_key: 'VISITOR_COOKIES_POLICY_URLS', titles_key: 'VISITOR_COOKIES_POLICY_TITLES' },
+  { name: 'privacySettingsManager', urls_key: 'VISITOR_PRIVACY_SETTINGS_URLS', titles_key: 'VISITOR_PRIVACY_SETTINGS_TITLES' },
+  { name: 'privacyPolicyManager', urls_key: 'VISITOR_PRIVACY_POLICY_URLS', titles_key: 'VISITOR_PRIVACY_POLICY_TITLES' },
+  { name: 'addPageManager', urls_key: 'PAGE_CREATE_URLS', titles_key: 'PAGE_CREATE_TITLES' } 
+]
 
-export function termsOfServiceManager(props) {
-  const { langs, urls, titles, clientUrl, changeRoute, langHandler } = props
-  const classNames = { container: 'link' }
-  const hrefLang = langHandler(langs)
-  const href = `${clientUrl}/${langHandler(urls.VISITOR_TERMS_OF_SERVICE_URLS)}`
-  const title = langHandler(titles.VISITOR_TERMS_OF_SERVICE_TITLES)
-  const label = title
-  const onClick = () => changeRoute({ href })
-  
-  return { classNames, href, hrefLang, title, label, onClick }
-}
+const managers = {}
 
-export function cookiesPolicyManager(props) {
-  const { langs, urls, titles, clientUrl, changeRoute, langHandler } = props
-  const classNames = { container: 'link' }
-  const hrefLang = langHandler(langs)
-  const href = `${clientUrl}/${langHandler(urls.VISITOR_COOKIES_POLICY_URLS)}`
-  const title = langHandler(titles.VISITOR_COOKIES_POLICY_TITLES)
-  const label = title
-  const onClick = () => changeRoute({ href })
-  
-  return { classNames, href, hrefLang, title, label, onClick }
-}
+links_data.map(({ name, urls_key, titles_key }) => {
+  managers[name] = props => {
+    const { langs, urls, titles, clientUrl, changeRoute, langHandler } = props
+    const classNames = { container: 'link' }
+    const hrefLang = langHandler(langs)
+    const href = `${clientUrl}/${langHandler(urls[urls_key])}`
+    const title = langHandler(titles[titles_key])
+    const label = title
+    const onClick = () => changeRoute({ href })
+    
+    return { classNames, href, hrefLang, title, label, onClick }
+  }
+})
 
-export function privacySettingsManager(props) {
-  const { langs, urls, titles, clientUrl, changeRoute, langHandler } = props
-  const classNames = { container: 'link' }
-  const hrefLang = langHandler(langs)
-  const href = `${clientUrl}/${langHandler(urls.VISITOR_PRIVACY_SETTINGS_URLS)}`
-  const title = langHandler(titles.VISITOR_PRIVACY_SETTINGS_TITLES)
-  const label = title
-  const onClick = () => changeRoute({ href })
-  
-  return { classNames, href, hrefLang, title, label, onClick }
-}
-
-export function privacyPolicyManager(props) {
-  const { langs, urls, titles, clientUrl, changeRoute, langHandler } = props
-  const classNames = { container: 'link' }
-  const hrefLang = langHandler(langs)
-  const href = `${clientUrl}/${langHandler(urls.VISITOR_PRIVACY_POLICY_URLS)}`
-  const title = langHandler(titles.VISITOR_PRIVACY_POLICY_TITLES)
-  const label = title
-  const onClick = () => changeRoute({ href })
-  
-  return { classNames, href, hrefLang, title, label, onClick }
-}
-
-export function addPageManager(props) {
-  const { langs, urls, titles, clientUrl, changeRoute, langHandler } = props
-  const classNames = { container: 'link' }
-  const hrefLang = langHandler(langs)
-  const href = `${clientUrl}/${langHandler(urls.PAGE_CREATE_URLS)}`
-  const title = langHandler(titles.PAGE_CREATE_TITLES)
-  const label = title
-  const onClick = () => changeRoute({ href })
-  
-  return { classNames, href, hrefLang, title, label, onClick }
-}
+export default managers
