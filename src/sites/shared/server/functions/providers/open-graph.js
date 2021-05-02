b9d513811598295b73393433bfd0181799cf4470
tom-ash <tomasz.bogusz@outlook.com>
2021-05-02 08:55:48 +0200

    FIX openGraph

--- src/sites/shared/server/functions/providers/open-graph.js
+++ src/sites/shared/server/functions/providers/open-graph.js
@@ -1,5 +1,7 @@
 function openGraphProvider(openGraph) {
   let openGraphHtml = ''
+
+  if (!openGraph) return openGraphHtml
   
   openGraph.map(({ name, value }) => {
     openGraphHtml += `<meta property="${name}" content="${value}" />`
