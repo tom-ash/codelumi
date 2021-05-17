b96aac28f1fcf6c9e1549497f63ec73b45b6ca18
tom-ash <tomasz.bogusz@outlook.com>
2021-05-17 07:13:24 +0200

    CHG shared contact

--- src/sites/shared/app/components/visitor/components/contact/contact.js
+++ src/sites/shared/app/components/visitor/components/contact/contact.js
@@ -1,22 +1,31 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { mapStateToProps, mapDispatchToProps } from './constants/mappers'
-import withStyles from 'isomorphic-style-loader/withStyles'
-import styles from './styles/styles.scss'
+import useStyles from 'isomorphic-style-loader/useStyles'
+import { CONTACT_COMPANY, CONTACT_ADDRESS_STREET, CONTACT_ADDRESS_POSTAL_CODE_AND_CITY, CONTACT_EMAIL } from './constants/texts.js'
 
-class VisitorContact extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  
-  render() {
-    const { langHandler } = this.props
-    return(
-      <div id='visitor-contact'>
-        VISITOR CONTACT
+const VisitorContact = props => {
+  const { appName, langHandler } = props
+  const styles = require(`../../../../../../${appName}/app/components/visitor/styles/styles.scss`)
+
+  useStyles(styles)
+
+  return(
+    <div className='legal-document'>
+      <div className='company'>
+        {CONTACT_COMPANY}
       </div>
-    )
-  }
+      <div className='address'>
+        <div className='street'>
+          {langHandler(CONTACT_ADDRESS_STREET)}
+        </div>
+        <div className='postalc-code-and-city'>
+          {langHandler(CONTACT_ADDRESS_POSTAL_CODE_AND_CITY)}
+        </div>
+      </div>
+      <div className='email'>
+        Email: {langHandler(CONTACT_EMAIL)}
+      </div>
+    </div>
+  )
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(VisitorContact))
+export default VisitorContact
