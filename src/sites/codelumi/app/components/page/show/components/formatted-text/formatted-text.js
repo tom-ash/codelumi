6cc9c6fa393aec21bed6a2044ed5ef2dd1c75137
tom-ash <tomasz.bogusz@outlook.com>
2021-05-18 17:50:07 +0200

    CHG codelumi formatted text

---
+++ src/sites/codelumi/app/components/page/show/components/formatted-text/formatted-text.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import useStyles from 'isomorphic-style-loader/useStyles'
+import styles from './styles/styles.scss'
+
+
+const FormattedText = props => {
+  const { nodeContent } = props
+
+  useStyles(styles)
+
+  return (
+    <div
+      className='formatted-text'
+      dangerouslySetInnerHTML={{__html: nodeContent.replace(/`([^`]+)`/g, (fullMatch, innerMatch) => (
+        `<span class='inner-code'>${innerMatch}</span>`
+      ))}}
+    />
+  )
+}
+
+export default FormattedText
--- src/sites/codelumi/app/components/page/show/functions/custom-node-parser.js
+++ src/sites/codelumi/app/components/page/show/functions/custom-node-parser.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import FormattedText from '../components/formatted-text/formatted-text.js'
 import CodeNode from '../components/code-node/code-node.js'
 import CodeLink from '../components/code-link/code-link.js'
 
 function customNodeParser(props) {
   const { nodeTag, index } = props
 
-  if (nodeTag === 'c') return <CodeNode {...props} key={index} />
-  if (nodeTag === 'lc') return <CodeLink {...props} key={index} />
+  if (nodeTag === 'c') return <CodeNode {...props} key={index}/>
+  if (nodeTag === 'lc') return <CodeLink {...props} key={index}/>
+  if (nodeTag === 'ft') return <FormattedText {...props} key={index}/>
 
   return null
 }
