2c7e8ca1ae5b948790260b6a39f2dc4525ab17a0
tom-ash <tomasz.bogusz@outlook.com>
2021-06-05 13:40:28 +0200

    ADD Table of Contents

--- src/sites/codelumi/app/components/page/show/functions/custom-meta-parser.js
+++ src/sites/codelumi/app/components/page/show/functions/custom-meta-parser.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import scrollToElement from '../../../../../../shared/app/functions/screen/scrollers/to-element.js'
 
 function customMetaParser({ meta, jsonBody }) {
   const jsonMeta = (function() {
@@ -21,9 +22,25 @@ function customMetaParser({ meta, jsonBody }) {
     return (
       <div className='table-of-contents-container'>
         <div className='table-of-contents'>
+          <div className='title'>
+            Table of Contents
+          </div>
           {headers.map((header, index) => {
             return (
-              <div key={index} className={header.t}>
+              <div
+                key={index}
+                className={header.t}
+                onClick={() => {
+                  const nodes = document.querySelectorAll(header.t)
+
+                  for (let i = 0; i < nodes.length; i++) {
+                    if (nodes[i].textContent === header.c) {
+                      scrollToElement(nodes[i], 30, -92)
+                      break
+                    }
+                  }
+                }}
+              >
                 {header.c}
               </div>
             )
