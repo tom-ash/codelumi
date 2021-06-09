355ca6a9be8f2b40a6453eb19365050d8e15499d
tom-ash <tomasz.bogusz@outlook.com>
2021-06-09 17:02:13 +0200

    ADD codelumi postgres category

--- src/sites/codelumi/app/components/page/show/components/code-link/constants/category-names.js
+++ src/sites/codelumi/app/components/page/show/components/code-link/constants/category-names.js
@@ -2,6 +2,7 @@ const categoryNames = {
   ruby: { en: 'Ruby', pl: 'Ruby' },
   js: { en: 'JavaScript', pl: 'JavaScript' },
   python: { en: 'Python', pl: 'Python' },
+  postgres: { en: 'PostgreSQL', pl: 'PostgreSQL' }
 }
 
 export default categoryNames
