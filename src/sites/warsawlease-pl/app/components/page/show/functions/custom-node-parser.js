188949bec77b69a22fe2637d736318c535a51206
tom-ash <tomasz.bogusz@outlook.com>
2021-07-09 14:50:50 +0200

    ADD warsawlease RichText

--- src/sites/warsawlease-pl/app/components/page/show/functions/custom-node-parser.js
+++ src/sites/warsawlease-pl/app/components/page/show/functions/custom-node-parser.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import RichText from '../components/rich-text/rich-text.js'
 
 function customNodeParser(props) {
+  const { nodeTag, index } = props
+
+  if (nodeTag === 'rt') return <RichText {...props} key={index}/>
+
   return null
 }
 
