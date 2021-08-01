fad0da9f51cbe57f99de49e776405546531504fe
tom-ash <tomasz.bogusz@outlook.com>
2021-08-01 10:04:16 +0200

    CHG codelumi codelink

--- src/sites/codelumi/app/components/page/show/components/code-link/functions/managers.js
+++ src/sites/codelumi/app/components/page/show/components/code-link/functions/managers.js
@@ -4,7 +4,7 @@ import categoryNames from '../constants/category-names.js'
 
 export const linkManager = props => {
   const { published, clientUrl, changeRoute, nodeContent } = props
-  const { pathname, hrefLang, category, title, icon, iconHref } = nodeContent
+  const { pathname, hrefLang, category, title, description, iconHref } = nodeContent
   const classNames = { container: `code-link ${category}` }
   const categoryName = categoryNames[category]
   if (!categoryName) return null
@@ -13,12 +13,16 @@ export const linkManager = props => {
   const href = `${clientUrl}/${pathname}`
   const label = (
     <>
-      <div className={`icon ${icon}`} style={{ backgroundImage: `url("${iconHref}")` }} />
-      <div className='category'>
-        <SVG name={category} /> {name}
+      <div className='picture'>
+        <img src={iconHref} />
       </div>
-      <div className='title'>
-        {title}
+      <div className='text'>
+        <div className='title'>
+          {title}
+        </div>
+        <div className='description'>
+          {description}
+        </div>
       </div>
     </>
   )
