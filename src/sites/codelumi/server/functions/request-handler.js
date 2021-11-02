caba83cd093a3b213e79d53473985fd0d61c5fb8
tom-ash <tomasz.bogusz@outlook.com>
2021-11-02 07:30:18 +0100

    Comment out redirects

--- src/sites/codelumi/server/functions/request-handler.js
+++ src/sites/codelumi/server/functions/request-handler.js
@@ -10,8 +10,8 @@ import CLIENT_URL from '../../shared/constants/urls/client.js'
 import API_URL from '../../shared/constants/urls/api.js'
 
 function handleRequest(req, res) {
-  if (req.hostname === 'codelumi.com' || req.hostname === 'www.codelumi.com') res.status(301).redirect(`https://${ROOT_DOMAIN}${req.originalUrl}`)
-  if (req.hostname === `www.${ROOT_DOMAIN}`) res.status(301).redirect(`https://${ROOT_DOMAIN}${req.originalUrl}`)
+  // if (req.hostname === 'codelumi.com' || req.hostname === 'www.codelumi.com') res.status(301).redirect(`https://${ROOT_DOMAIN}${req.originalUrl}`)
+  // if (req.hostname === `www.${ROOT_DOMAIN}`) res.status(301).redirect(`https://${ROOT_DOMAIN}${req.originalUrl}`)
 
   genericRequestHandler({
     req,
