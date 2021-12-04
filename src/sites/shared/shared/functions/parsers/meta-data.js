25619a22cd930578e019fa9242656d8d4c091bc4
tom-ash <tomasz.bogusz@outlook.com>
2021-12-04 11:20:14 +0100

    shared | Simplify metaDataParser

--- src/sites/shared/shared/functions/parsers/meta-data.js
+++ src/sites/shared/shared/functions/parsers/meta-data.js
@@ -3,24 +3,21 @@ import schemaOrgGenericParser from './schema-org-generic.js'
 
 function metaDataParser({
   lang,
-  title, titleParser, description, descriptionParser, keywords, keywordsParser, image, imageParser,
-  openGraph, openGraphSpecificParser,
-  schemaOrg, schemaOrgSpecificParser
+  title,
+  description,
+  keywords,
+  image,
+  openGraph,
+  schemaOrg
 }) {
-  const parsedTitle = titleParser && titleParser({ title, lang }) || title
-  const parsedDescription = descriptionParser && descriptionParser({ description, lang }) || description
-  const parsedKeywords = keywordsParser && keywordsParser({ keywords, lang }) || keywords
-  const parsedImage = imageParser && imageParser({ image, lang }) || image
-  const openGraphParser = openGraphSpecificParser || openGraphGenericParser
-  const parsedOpenGraph = openGraphParser({ title: parsedTitle, description: parsedDescription, keywords: parsedKeywords, image: parsedImage, ...openGraph })
-  const schemaParser = schemaOrgGenericParser || schemaOrgSpecificParser
-  const parsedSchemaOrg = schemaParser({ lang, title: parsedTitle, description: parsedDescription, keywords: parsedKeywords, ...schemaOrg })
+  const parsedOpenGraph = openGraphGenericParser({ title, description, keywords, image, ...openGraph })
+  const parsedSchemaOrg = schemaOrgGenericParser({ lang, title, description, keywords, ...schemaOrg })
 
   return {
     lang,
-    title: parsedTitle,
-    description: parsedDescription,
-    keywords: parsedKeywords,
+    title,
+    description,
+    keywords,
     openGraph: parsedOpenGraph,
     schemaOrg: parsedSchemaOrg
   }
