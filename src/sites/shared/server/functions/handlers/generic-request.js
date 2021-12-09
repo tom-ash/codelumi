88ed764273bc84c19381f1018c5cd31f385cfc3c
tom-ash <tomasz.bogusz@outlook.com>
2021-12-09 08:15:23 +0100

    shared | Add buildSitemap()

--- src/sites/shared/server/functions/handlers/generic-request.js
+++ src/sites/shared/server/functions/handlers/generic-request.js
@@ -2,6 +2,8 @@ import routeSender from '../senders/route.js'
 import getPureUrl from '../../../shared/functions/routes/getters/pure-url.js'
 import getDevice from '../../../shared/functions/getters/device.js'
 import getVisitorState from '../../../shared/functions/getters/visitor-state.js'
+import fetch from 'node-fetch'
+import buildSitemap from '../sitemaps/build.js'
 
 function genericRequestHandler({
   req, res,
@@ -15,6 +17,23 @@ function genericRequestHandler({
   const query = queryMatch ? queryMatch[0] : ''
   const device = getDevice(headers['user-agent'])
   const visitorState = getVisitorState(cookies)
+
+  if (url === 'sitemap1.xml') {
+    return fetch(`${apiUrl}/sitemap`, {
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+  
+      throw new Error('Sitemap built error!')
+    })
+    .then(unlocalizedUrlGroups => {
+      const sitemap = buildSitemap({ unlocalizedUrlGroups, clientUrl })
+
+      res.set('Content-Type', 'text/xml')
+      res.send(sitemap)
+    })
+  }
 
   routeSender({
     res,
---
+++ src/sites/shared/server/functions/sitemaps/build.js
@@ -0,0 +1,55 @@
+import pretty from 'pretty'
+
+function buildSitemap({ unlocalizedUrlGroups, clientUrl }) {
+  return (
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+      ${buildUrls({ unlocalizedUrlGroups, clientUrl })}
+    </urlset>`
+  )
+}
+
+function buildUrls({ unlocalizedUrlGroups, clientUrl }) {
+  let urls = ''
+
+  unlocalizedUrlGroups.map(unlocalizedUrlGroup => {
+    urls += buildLocalizedUrls({ unlocalizedUrlGroup, clientUrl })
+  })
+
+  // TODO Change to pretty xml.
+  return pretty(urls)
+}
+
+function buildLocalizedUrls({ unlocalizedUrlGroup, clientUrl }) {
+  let localizedUrls = ''
+
+  unlocalizedUrlGroup.map(localizedUrl => {
+    localizedUrls += (
+      `<url>
+        <loc>${clientUrl}/${localizedUrl.path}</loc>
+        <changefreq>${localizedUrl.changefreq}</changefreq>
+        <priority>${localizedUrl.priority}</priority>
+        ${buildAltUrls({ unlocalizedUrlGroup, clientUrl })}
+      </url>`
+    )
+  })
+
+  return localizedUrls
+}
+
+function buildAltUrls({ unlocalizedUrlGroup, clientUrl }) {
+  let altLocalizedUrls = ''
+  
+  unlocalizedUrlGroup.map(localizedUrl => {
+    altLocalizedUrls += (
+      `<xhtml:link
+         rel="alternate"
+         href="${clientUrl}/${localizedUrl.path}"
+         hreflang="${localizedUrl.lang}" />`
+    )
+  })
+
+  return altLocalizedUrls
+}
+
+export default buildSitemap
