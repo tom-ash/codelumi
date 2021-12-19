0bfd2cdedf285f16049bb38990aae4ef617ce922
tom-ash <tomasz.bogusz@outlook.com>
2021-12-19 08:14:45 +0100

    warsawlease.pl | Make :map urls canonical to :catalogue

--- src/sites/shared/app/functions/routes/setters/meta.js
+++ src/sites/shared/app/functions/routes/setters/meta.js
@@ -5,12 +5,18 @@ function metaSetter(meta) {
   const { clientUrl, url, canonicalUrl } = meta
   const parsedMeta = metaDataParser(meta)
   const { lang, title, description, keywords, openGraph, schemaOrg } = parsedMeta
+  const canonical = typeof canonicalUrl === 'string' ? canonicalUrl : url
+
+  console.log(meta)
+  console.log(canonical)
+  console.log(canonicalUrl)
+  console.log(typeof canonicalUrl)
 
   document.documentElement.lang = lang
   document.title = title
   document.querySelector('meta[name="description"]').setAttribute("content", description)
   document.querySelector('meta[name="keywords"]').setAttribute("content", keywords)
-  document.querySelector('link[rel="canonical"]').setAttribute("href", getPureUrl(`${clientUrl}/${canonicalUrl || url}`))
+  document.querySelector('link[rel="canonical"]').setAttribute("href", getPureUrl(`${clientUrl}/${canonical}`))
 
   openGraph.map(({ name, value }) => {
     document.querySelector(`meta[property="${name}"]`).setAttribute("content", value)
