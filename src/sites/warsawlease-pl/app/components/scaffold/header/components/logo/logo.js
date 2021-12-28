4d8280fef8fd766eb7d72b124f6932f0930badee
tom-ash <tomasz.bogusz@outlook.com>
2021-12-28 08:20:44 +0100

    warsawlease.pl | Add mapawynajmu.pl logo

--- src/sites/warsawlease-pl/app/components/scaffold/header/components/logo/logo.js
+++ src/sites/warsawlease-pl/app/components/scaffold/header/components/logo/logo.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import useStyles from 'isomorphic-style-loader/useStyles'
-import LogoHtml from '../../../../../assets/logo/logo.html'
-import styles from '../../../../../assets/logo/styles/styles.css'
+// import LogoHtml from '../../../../../assets/logo/logo.html'
+// import styles from '../../../../../assets/logo/styles/styles.css'
+
+// Old Logo
+import styles from '../../../../../assets/old-logo/styles/styles.css'
+import OldLogoHtml from '../../../../../assets/old-logo/old-logo.html'
 
 const Logo = () => {
   useStyles(styles)
 
-  return <div dangerouslySetInnerHTML={{__html: LogoHtml}} />
+  return <div dangerouslySetInnerHTML={{__html: OldLogoHtml}} />
 }
 
 export default Logo
