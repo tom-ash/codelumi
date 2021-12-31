e1dd219aad29725a02e3931fe7acd2f7519d3c7a
tom-ash <tomasz.bogusz@outlook.com>
2021-12-31 13:19:44 +0100

    soundof.it | fix prod api url

--- src/sites/soundof-it/shared/functions/getters/api-url.js
+++ src/sites/soundof-it/shared/functions/getters/api-url.js
@@ -1,7 +1,7 @@
 function getApiUrl() {
   switch (APP_ENV) {
     case 'development': return 'http://localhost:3001/soundof_it'
-    case 'production': return 'https://soundof-it-red.herokuapp.com/warsawlease'
+    case 'production': return 'https://soundof-it-red.herokuapp.com/soundof_it'
     default: return ''
   }
 }
