7739ffd106b34cd660342517d092e073fae6d3de
tom-ash <tomasz.bogusz@outlook.com>
2021-12-31 13:09:25 +0100

    soundof.it | codelumi -> soundof-it (api urls)

--- src/sites/soundof-it/shared/functions/getters/api-url.js
+++ src/sites/soundof-it/shared/functions/getters/api-url.js
@@ -1,7 +1,7 @@
 function getApiUrl() {
   switch (APP_ENV) {
     case 'development': return 'http://localhost:3001/soundof_it'
-    case 'production': return 'https://codelumired.herokuapp.com/soundof_it'
+    case 'production': return 'https://soundof-it-red.herokuapp.com/warsawlease'
     default: return ''
   }
 }
