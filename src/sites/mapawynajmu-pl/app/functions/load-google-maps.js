456d18d455782702af544665d62f504df932d214
tom-ash <tomasz.bogusz@outlook.com>
2022-01-09 09:55:50 +0100

    mapawynajmu.pl | Change google api key

--- src/sites/mapawynajmu-pl/app/functions/load-google-maps.js
+++ src/sites/mapawynajmu-pl/app/functions/load-google-maps.js
@@ -1,6 +1,6 @@
 import { loadScript } from './load-script'
 
-const apiKey = 'AIzaSyBTV8VlJwqOP4aGz0SvDWJyEFloZh9YjgA'
+const apiKey = 'AIzaSyAUEJ1HRdOeh_QKQTUU-sCAgnerzNJY-8k'
 
 export function initializeGoogleMaps() {
   loadScript(`https://maps.googleapis.com/maps/api/js?key=${apiKey}`)
