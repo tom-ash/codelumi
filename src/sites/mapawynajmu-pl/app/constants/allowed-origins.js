8952c2ac13423ff482d6e0299bf701aa2e61d521
tom-ash <tomasz.bogusz@outlook.com>
2022-01-15 10:08:37 +0100

    mapawynajmu.pl | Add mapawynajmu.pl to allowed origins

--- src/sites/mapawynajmu-pl/app/constants/allowed-origins.js
+++ src/sites/mapawynajmu-pl/app/constants/allowed-origins.js
@@ -1,5 +1,7 @@
 export const allowedOrigins = [
   'https://www.warsawlease.pl',
   'https://warsawlease.pl',
-  'http://local.warsawlease.pl:8080'
+  'http://local.warsawlease.pl:8080',
+  'https://mapawynajmu.pl',
+  'http://local.mapawynajmu.pl:8080'
 ]
