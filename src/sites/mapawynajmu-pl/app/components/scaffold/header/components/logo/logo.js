14006a4102ecf92aa234d32c683069bb7392445b
tom-ash <tomasz.bogusz@outlook.com>
2022-02-01 14:09:34 +0100

    mapawynajmu.pl | Use mapawynajmu.pl logo

--- src/sites/mapawynajmu-pl/app/components/scaffold/header/components/logo/logo.js
+++ src/sites/mapawynajmu-pl/app/components/scaffold/header/components/logo/logo.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import useStyles from 'isomorphic-style-loader/useStyles'
-// import LogoHtml from '../../../../../assets/logo/logo.html'
-// import styles from '../../../../../assets/logo/styles/styles.css'
-
-// Old Logo
-import styles from '../../../../../assets/old-logo/styles/styles.css'
-import LogoHtml from '../../../../../assets/old-logo/old-logo.html'
+import LogoHtml from '../../../../../assets/logo/logo.html'
+import styles from '../../../../../assets/logo/styles/styles.css'
 
 const Logo = () => {
   useStyles(styles)
--- src/sites/mapawynajmu-pl/shared/constants/domains/root.js
+++ src/sites/mapawynajmu-pl/shared/constants/domains/root.js
@@ -1,8 +1,5 @@
 import APEX_DOMAIN from "./apex"
 
-const ROOT_DOMAIN = `www.${APEX_DOMAIN}`
-
-// TODO: rebranding
-// const ROOT_DOMAIN = APEX_DOMAIN
+const ROOT_DOMAIN = APEX_DOMAIN
 
 export default ROOT_DOMAIN
