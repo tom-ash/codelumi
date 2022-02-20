233bdc7ef929df7b97d0280e0287bbd8570ccb7e
tom-ash <tomasz.bogusz@outlook.com>
2022-02-20 10:07:26 +0100

    soundof.IT | Revamp CodeLink styles

--- src/sites/soundof-it/app/components/page/show/components/code-link/functions/managers.js
+++ src/sites/soundof-it/app/components/page/show/components/code-link/functions/managers.js
@@ -2,8 +2,19 @@ import React from 'react'
 import categoryNames from '../constants/category-names.js'
 
 export const linkManager = props => {
-  const { clientUrl, changeRoute, nodeContent } = props
-  const { pathname, hrefLang, category, title, description, iconHref } = nodeContent
+  const {
+    clientUrl,
+    changeRoute,
+    nodeContent
+  } = props
+  const {
+    pathname,
+    hrefLang,
+    category,
+    title,
+    image,
+    logo
+  } = nodeContent
   const classNames = { container: `code-link ${category}` }
   const categoryName = categoryNames[category]
   if (!categoryName) return null
@@ -11,7 +22,8 @@ export const linkManager = props => {
   const href = `${clientUrl}/${pathname}`
   const label = (
     <>
-      {iconHref && <img src={iconHref} />}  
+      <img className='image' src={image} />
+      <img className='logo' src={logo} />
       <div className='title'>
         {title}
       </div>
