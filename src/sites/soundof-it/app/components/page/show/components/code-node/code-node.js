e6edb4d3225980fee1385ce671169118dddf1bce
tom-ash <tomasz.bogusz@outlook.com>
2022-02-21 08:49:25 +0100

    shared | Add xml to CodeNode

--- src/sites/soundof-it/app/components/page/show/components/code-node/code-node.js
+++ src/sites/soundof-it/app/components/page/show/components/code-node/code-node.js
@@ -7,8 +7,9 @@ hljs.registerLanguage('ruby', require('highlight.js/lib/languages/ruby'))
 hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
 hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
 hljs.registerLanguage('pgsql', require('highlight.js/lib/languages/pgsql'))
+hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
 
-const AVAILABLE_LANGUAGES = ['javascript', 'ruby', 'sql', 'pgsql']
+const AVAILABLE_LANGUAGES = ['javascript', 'ruby', 'sql', 'pgsql', 'xml']
 
 class CodeNode extends React.Component {
   constructor(props) {
