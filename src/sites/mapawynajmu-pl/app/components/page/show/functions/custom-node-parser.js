d95a635ca6154e0ef2114fd5799d3f509ee0cc19
tom-ash <tomasz.bogusz@outlook.com>
2022-02-22 08:35:49 +0100

    mapawynajmu.pl | Remove custom PictureHeader

--- src/sites/mapawynajmu-pl/app/components/page/show/functions/custom-node-parser.js
+++ src/sites/mapawynajmu-pl/app/components/page/show/functions/custom-node-parser.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import ArticleLink from '../components/article-link/article-link.js'
-import PictureHeader from '../components/picture-header/picture-header.js'
 
 function customNodeParser(props) {
   const { nodeTag, index } = props
 
   if (nodeTag === 'al') return <ArticleLink {...props} key={index}/>
-  if (nodeTag === 'pt') return <PictureHeader {...props} key={index}/>
 
   return null
 }
