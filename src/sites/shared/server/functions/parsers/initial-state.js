37fb65cef6beb1c05acfbac64e585554cebbd63a
tom-ash <tomasz.bogusz@outlook.com>
2022-03-20 14:11:00 +0100

    shared | Adjust initialStateParser to two part track state keys

--- src/sites/shared/server/functions/parsers/initial-state.js
+++ src/sites/shared/server/functions/parsers/initial-state.js
@@ -4,36 +4,55 @@ function initialStateParserV2(state) {
   Object.keys(state).map(stateKey => {
     if (!stateKey.match('/')) return
 
+    let mainKey
+    let middleKey
+    let detailKey
     const nestedKeys = stateKey.split('/')
-    const mainKey = nestedKeys[nestedKeys.length - 3]
-    const middleKey = nestedKeys[nestedKeys.length - 2]
-    const detailKey = nestedKeys[nestedKeys.length - 1]
-    const mainObject = parsedState[mainKey]
 
-    // TODO: ABSTRACT
-    if (stateKey == 'assets/svgs') return parsedState = { ...parsedState, [middleKey]: { [detailKey]: state[stateKey]}}
+    if (nestedKeys.length === 3) {
+      mainKey = nestedKeys[nestedKeys.length - 3]
+      middleKey = nestedKeys[nestedKeys.length - 2]
+      detailKey = nestedKeys[nestedKeys.length - 1]
 
-    if (!mainObject) {
-      return parsedState = { ...parsedState, [mainKey]: { [middleKey]: { [detailKey]: state[stateKey] }}}
-    }
-
-    const middleObj = parsedState[mainKey][middleKey]
-    if (!middleObj) {
-      return parsedState = {
-        ...parsedState,
-        [mainKey]: { ...mainObject, [middleKey]: { [detailKey]: state[stateKey] }}
+      const mainObject = parsedState[mainKey]
+      if (!mainObject) {
+        return parsedState = { ...parsedState, [mainKey]: { [middleKey]: { [detailKey]: state[stateKey] }}}
       }
-    }
-
-    const detailObj = parsedState[mainKey][middleKey][detailKey]
-    if (!detailObj) {
-      return parsedState = {
-        ...parsedState,
-        [mainKey]: {
-          ...mainObject, [middleKey]: {
-            ...middleObj,
-            [detailKey]: state[stateKey]
+  
+      const middleObj = parsedState[mainKey][middleKey]
+      if (!middleObj) {
+        return parsedState = {
+          ...parsedState,
+          [mainKey]: { ...mainObject, [middleKey]: { [detailKey]: state[stateKey] }}
+        }
+      }
+  
+      const detailObj = parsedState[mainKey][middleKey][detailKey]
+      if (!detailObj) {
+        return parsedState = {
+          ...parsedState,
+          [mainKey]: {
+            ...mainObject, [middleKey]: {
+              ...middleObj,
+              [detailKey]: state[stateKey]
+            }
           }
+        }
+      }
+    } else if (nestedKeys.length === 2) {
+      mainKey = nestedKeys[nestedKeys.length - 2]
+      middleKey = nestedKeys[nestedKeys.length - 1]
+
+      const mainObject = parsedState[mainKey]
+      if (!mainObject) {
+        return parsedState = { ...parsedState, [mainKey]: { [middleKey]: state[stateKey]}}
+      }
+  
+      const middleObj = parsedState[mainKey][middleKey]
+      if (!middleObj) {
+        return parsedState = {
+          ...parsedState,
+          [mainKey]: { ...mainObject, [middleKey]: state[stateKey]}
         }
       }
     }
