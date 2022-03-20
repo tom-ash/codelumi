72d1fe46bacab36dc0748666726028b737a0773c
tom-ash <tomasz.bogusz@outlook.com>
2022-03-20 08:17:39 +0100

    soundof.IT | job/create/form | Add create example job

--- src/sites/soundof-it/app/components/announcement/components/create/components/form/functions/submit.ts
+++ src/sites/soundof-it/app/components/announcement/components/create/components/form/functions/submit.ts
@@ -1,4 +1,10 @@
 import apiUrl from '../../../../../../../../shared/constants/urls/api.js'
+// import getCookieValue from
+import getAccessToken from '../../../../../../../../../mapawynajmu-pl/app/components/user/components/authorize/components/tokens/functions/get-tokens.js'
+
+// import getCookieValue
+
+// import getAccessToken from '../../../../user/components/authorize/components/tokens/functions/get-tokens'
 
 type submitProps = {
   selectedSkills: Array<any>
@@ -8,9 +14,11 @@ const submit = (props: submitProps) => {
   const { selectedSkills } = props
   const body = { selectedSkills }
 
+  console.log(getAccessToken())
+
   fetch(apiUrl + '/job/create/as-user', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 'Content-Type': 'application/json', 'Access-Token': getAccessToken() },
     body: JSON.stringify(body)
   })
   .then(response => {
