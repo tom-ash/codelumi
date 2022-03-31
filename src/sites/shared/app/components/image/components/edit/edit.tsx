b04375d42d94ecbdbda8b2b89d97aa783c1e64b9
tom-ash <tomasz.bogusz@outlook.com>
2022-03-31 08:48:46 +0200

    shared | Add image/edit bodyElements

--- src/sites/shared/app/components/image/components/edit/edit.tsx
+++ src/sites/shared/app/components/image/components/edit/edit.tsx
@@ -15,11 +15,75 @@ type ImageEditProps = {
 const ImageEdit = (props: ImageEditProps) => {
   useStyles(styles)
 
-  console.log("HERE")
+  const bodyElements = [
+
+    {
+      t: 'img',
+      attrs: {
+        src: 'https://soundofit.s3.eu-central-1.amazonaws.com/assets/backgrounds/gradient-yellow-brown-lines.jpg',
+        style: {
+          width: 1200,
+          height: 630
+        }
+      }
+    },
+    {
+      t: 'img',
+      attrs: {
+        src: 'https://soundofit.s3.eu-central-1.amazonaws.com/assets/logos/javascript.png',
+        style: {
+          width: 200,
+          zIndex: 2,
+          position: 'absolute',
+          top: 150,
+          right: 0,
+          left: 0,
+          margin: '0 auto',
+        }
+      }
+    },
+    {
+      t: 'div',
+      attrs: {
+        style: {
+          zIndex: 2,
+          color: 'white',
+          position: 'absolute',
+          right: 0,
+          bottom: 100,
+          left: 0,
+          margin: '0 auto',
+          textAlign: 'center',
+          fontSize: 50,
+          fontFamily: 'Titillium Web'
+        }
+      },
+      c: 'JavaScript Tutorial'
+    }
+  ]
+
 
   return (
     <div className='edit'>
-      <div className='output' />
+      <div id='image-output' className='output'>
+        {bodyElements.map(bodyElement => {
+
+
+          return (
+          React.createElement(
+            bodyElement.t,
+            //@ts-ignore
+            bodyElement.attrs || {},
+            //@ts-ignore
+            bodyElement.c
+          )
+          )
+
+
+
+        })}
+
+      </div>
 
       <div className='input'>
         <Body />
