3153f61dfa643e9dcfdf6da1e7bac03645580d7e
tom-ash <tomasz.bogusz@outlook.com>
2022-04-12 08:47:21 +0200

    soundof.IT | Change TableOfContents styles

--- src/sites/shared/app/components/page/components/shared/components/tile/components/table-of-contents/table-of-contents.js
+++ src/sites/shared/app/components/page/components/shared/components/tile/components/table-of-contents/table-of-contents.js
@@ -10,48 +10,50 @@ const TableOfContents = (props) => {
 
   return (
     <nav className='table-of-contents'>
-      <h2>
-        {nodeContent}
-      </h2>
-      <ul>
-        {headers.map((header, index) => {
-          const headerContent = header.c
-          const headerNumber = header.n
+      <div className='inner-container'>
+        <h2>
+          {nodeContent}
+        </h2>
+        <ul>
+          {headers.map((header, index) => {
+            const headerContent = header.c
+            const headerNumber = header.n
 
-          return (
-            <li key={index}>
-              <a
-                key={index}
-                className={header.t}
-                href={`#${urlify(headerContent)}`}
-                onClick={e => {
-                  e.preventDefault()
+            return (
+              <li key={index}>
+                <a
+                  key={index}
+                  className={header.t}
+                  href={`#${urlify(headerContent)}`}
+                  onClick={e => {
+                    e.preventDefault()
 
-                  history.pushState(null, null, `${window.location.pathname}#${urlify(headerContent)}`)
+                    history.pushState(null, null, `${window.location.pathname}#${urlify(headerContent)}`)
 
-                  const targetHeader = headers.find(headerNode => urlify(headerContent) === urlify(headerNode.c))
-                  const domHeaders = document.getElementsByTagName('h2')
-                  let element
+                    const targetHeader = headers.find(headerNode => urlify(headerContent) === urlify(headerNode.c))
+                    const domHeaders = document.getElementsByTagName('h2')
+                    let element
 
-                  for (var i = 0; i < domHeaders.length; i++) {
-                    if (domHeaders[i].textContent == targetHeader.c) {
-                      element = domHeaders[i]
-                      break
+                    for (var i = 0; i < domHeaders.length; i++) {
+                      if (domHeaders[i].textContent == targetHeader.c) {
+                        element = domHeaders[i]
+                        break
+                      }
                     }
-                  }
 
-                  const translation = -80
-                  setScreenOffsetAtElement({ element, translation })
-                }}
-              >
-                {headerNumber ?
-                <><span>{headerNumber}</span><span>{headerContent}</span></>
-                : headerContent}
-              </a>
-            </li>
-          )
-        })}
-      </ul>
+                    const translation = -80
+                    setScreenOffsetAtElement({ element, translation })
+                  }}
+                >
+                  {headerNumber ?
+                  <><span>{headerNumber}</span><span>{headerContent}</span></>
+                  : headerContent}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </nav>
   )
 }
