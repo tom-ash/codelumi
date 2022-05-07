02a97050e054c09b3bdf09d2395709149bd9fe5e
tom-ash <tomasz.bogusz@outlook.com>
2022-05-07 11:17:36 +0200

    soundof.IT | page/index change CodeLink style

--- src/sites/soundof-it/app/components/page/show/components/code-link/functions/managers.js
+++ src/sites/soundof-it/app/components/page/show/components/code-link/functions/managers.js
@@ -16,10 +16,16 @@ export const linkManager = props => {
   const href = `${clientUrl}/${pathname}`
   const label = (
     <>
-      <img className='image' src={image} />
-      <img className='logo' src={logo} />
-      <div className='title'>
-        {title}
+      <div className='logo'>
+        <img src={logo} />
+      </div>
+      <div className='title-and-meta'>
+        <div className='title'>
+          {title}
+        </div>
+        <div className='last-updated-at'>
+          January 24<sup>th</sup>, 2022
+        </div>
       </div>
     </>
   )
