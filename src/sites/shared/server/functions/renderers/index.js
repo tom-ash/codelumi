6889d6cac4e719bb15f0f8cc38099203e42f9c71
tom-ash <tomasz.bogusz@outlook.com>
2022-06-05 16:41:50 +0200

    shared | adjust indexRenderer

--- src/sites/shared/server/functions/renderers/index.js
+++ src/sites/shared/server/functions/renderers/index.js
@@ -3,42 +3,33 @@ import getPureUrl from '../../../shared/functions/routes/getters/pure-url.js'
 import buildAlternateLinks from '../builders/alternate_links.js'
 
 function indexRenderer({
-  clientUrl, url, canonicalUrl, noIndex, links, langs, lang, buildUrl,
-  title, description, keywords, openGraph, schema,
+  lang, canonicalUrl, alternateLinks,
+  title, robots, description, keywords,
+  openGraph, schema,
   html, css, preloadedState, scriptTags
 }) {
-  const charsetMeta = `<meta charset="UTF-8">`
-  const viewportMeta = `<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">`
-  const canonicalPath = typeof canonicalUrl === 'string' ? canonicalUrl : url
-  const canonicalMeta = `<link rel="canonical" href="${getPureUrl(`${clientUrl}/${canonicalPath}`)}">`
-  const robotsMeta = `<meta name="robots" content="${noIndex ? 'noindex' : 'index,follow,all'}">`
-  const titleMeta = `<title>${title}</title>`
-  const descriptionMeta = `<meta name="description" content="${description}">`
-  const keywordsMeta = `<meta name="keywords" content="${keywords}">`
-  const schemaOrgMeta = `<script type="application/ld+json">${schema}</script>`
-  const style = `<style type="text/css">${[...css].join('')}</style>`
-  const preloadedStateScript = `<script>window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}</script>`
-  const alternateLinks = buildAlternateLinks({ links, langs, buildUrl })
+  // const alternateLinks = buildAlternateLinks({ links, langs, buildUrl })
+  
   const indexAsHtml = (
     `<!doctype html>
     <html lang="${lang}">
       <head>
-        ${charsetMeta}
-        ${viewportMeta}
-        ${canonicalMeta}
+        <link rel="canonical" href="${canonicalUrl}">
         ${alternateLinks}
-        ${robotsMeta}
-        ${titleMeta}
-        ${descriptionMeta}
-        ${keywordsMeta}
+        <title>${title}</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="robots" content="${robots}">
+        <meta name="description" content="${description}">
+        <meta name="keywords" content="${keywords}">
         ${openGraph}
-        ${schemaOrgMeta}
-        ${style}
+        <script type="application/ld+json">${schema}</script>
+        <style type="text/css">${[...css].join('')}</style>
       </head>
       <body>
         ${html}
         ${scriptTags}
-        ${preloadedStateScript}
+        <script>window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}</script>
       </body>
     </html>`
   )
