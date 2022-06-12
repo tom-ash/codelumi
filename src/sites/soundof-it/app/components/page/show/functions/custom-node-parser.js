ed9bb3de4e89481c8def818121f937511a1704d6
tom-ash <tomasz.bogusz@outlook.com>
2022-06-12 11:25:18 +0200

    soundof.it | page/index remove custom PageIndexer

--- src/sites/soundof-it/app/components/page/show/functions/custom-node-parser.js
+++ src/sites/soundof-it/app/components/page/show/functions/custom-node-parser.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import CodeNode from '../components/code-node/code-node.js'
-import PageIndexer from '../components/page-indexer/page-indexer'
 
 function customNodeParser(props) {
   const { nodeTag, index, codeLang } = props
 
   if (nodeTag === 'c') return <CodeNode {...{ ...props, key: index }} />
-  if (nodeTag === 'PageIndexer') return <PageIndexer {...{ ...props, key: index }} />
 
   return null
 }
