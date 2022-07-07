6b9b4acaaae817846ef771f8df4d90a6272b0d0b
tom-ash <tomasz.bogusz@outlook.com>
2022-07-07 07:47:03 +0200

    shared | page/tile adjust Image

---
+++ src/sites/shared/app/components/page/components/shared/components/tile/components/node-parser/components/image/image.d.tsx
@@ -0,0 +1,4 @@
+interface ImageProps {
+  src: string,
+  alt: string
+}
--- src/sites/shared/app/components/page/components/shared/components/tile/components/node-parser/components/image/image.tsx
+++ src/sites/shared/app/components/page/components/shared/components/tile/components/node-parser/components/image/image.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
 
-interface ImageProps {
-  src: string,
-  alt: string
-}
-
 const Image = (props: ImageProps) => {
   const { src, alt } = props
   const imgProps = { src, alt }
