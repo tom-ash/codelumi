08126fd4310ab8ae98f94becbba0a6a8a3dc004d
tom-ash <tomasz.bogusz@outlook.com>
2022-07-17 14:49:09 +0200

    shared | page add shareOnFacebook to Share

--- src/sites/shared/app/components/support/render-node/components/share/share.tsx
+++ src/sites/shared/app/components/support/render-node/components/share/share.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import useStyles from 'isomorphic-style-loader/useStyles'
 import styles from './styles/styles.scss'
 import SVG from '../../../../../../../shared/app/components/support/svg/svg'
+import { shareOnFacebook } from '../../../../../../../mapawynajmu-pl/app/components/announcement/functions/share-on-facebook'
 
 const Share = (props: ShareProps) => {
   useStyles(styles)
@@ -12,7 +13,10 @@ const Share = (props: ShareProps) => {
 
   return (
     <div className='share'>
-      <div className='facebook'>
+      <div
+        className='facebook'
+        onClick={() => shareOnFacebook(href, false)}
+      >
         {/* @ts-ignore */}
         <SVG name='facebook' />
       </div>
