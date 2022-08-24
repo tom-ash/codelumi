1335b9857e911d2756dd4f7d0a453b5293816539
tom-ash <tomasz.bogusz@outlook.com>
2022-08-24 08:24:32 +0200

    mapawynajmu.pl | adjust header .add-announcement styles

--- src/sites/mapawynajmu-pl/shared/constants/routes/labels.js
+++ src/sites/mapawynajmu-pl/shared/constants/routes/labels.js
@@ -4,6 +4,6 @@ import {
 } from '../langs/langs'
 
 export const ANNOUNCEMENT_CREATE_LABELS = {
-  [PL]: 'Dodaj ogłoszenie',
-  [EN]: 'Add Announcement'
+  [PL]: 'Dodaj ogłoszenie za darmo',
+  [EN]: 'Add Listing for Free'
 }
