ec904605a7c18aca6efd31f189bf877ae6f617c0
tom-ash <tomasz.bogusz@outlook.com>
2022-09-10 11:45:45 +0200

    mapawynajmu.pl | add GoToLink extended

--- src/sites/mapawynajmu-pl/app/components/announcement/components/index/constants/state.js
+++ src/sites/mapawynajmu-pl/app/components/announcement/components/index/constants/state.js
@@ -14,35 +14,35 @@ export const control = {
   rebuildQueryParams: false
 }
 
-const tile = {
-  area: 58.2,
-  availabilityDate: '2023-01-01',
-  category: 7,
-  floor: 3,
-  name: 'Super',
-  link: 'asdadsa',
-  grossRentAmount: 2300,
-  grossRentAmountPerSqm: 39.51,
-  id: 29,
-  locality: "Warszawa",
-  netRentAmount: null,
-  netRentAmountPerSqm: null,
-  path: "39-przestrzen-coworkingowa-na-wynajem-warszawa-ochota",
-  pictures: [{ database: '20220320142047725oWSkWcommOF8-tB6FavuOQ.jpeg', description: '' }],
-  rentCurrency: 0,
-  rooms: 3,
-  show: true,
-  sublocality: "Ochota",
-  title: "Przestrzeń coworkingowa do wynajęcia, Warszawa, Ochota",
-  totalFloors: 5,
-  description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-}
+// const tile = {
+//   area: 58.2,
+//   availabilityDate: '2023-01-01',
+//   category: 7,
+//   floor: 3,
+//   name: 'Super',
+//   link: 'asdadsa',
+//   grossRentAmount: 2300,
+//   grossRentAmountPerSqm: 39.51,
+//   id: 29,
+//   locality: "Warszawa",
+//   netRentAmount: null,
+//   netRentAmountPerSqm: null,
+//   path: "39-przestrzen-coworkingowa-na-wynajem-warszawa-ochota",
+//   pictures: [{ database: '20220320142047725oWSkWcommOF8-tB6FavuOQ.jpeg', description: '' }],
+//   rentCurrency: 0,
+//   rooms: 3,
+//   show: true,
+//   sublocality: "Ochota",
+//   title: "Przestrzeń coworkingowa do wynajęcia, Warszawa, Ochota",
+//   totalFloors: 5,
+//   description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+// }
 
 export const data = {
   amount: null,
   announcements: null,
   tileId: null,
-  tile: tile,
+  tile: null,
   pins: null,
   apartmentsAmount: 0,
   housesAmount: 0,
