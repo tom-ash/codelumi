62e0bf10ae9181ca2cfb13c9b22a03ec395db3b0
tom-ash <tomasz.bogusz@outlook.com>
2022-09-20 07:30:44 +0200

    mapawynajmu.pl | visitor/contact change contact email

--- src/sites/shared/app/components/visitor/components/contact/constants/texts.js
+++ src/sites/shared/app/components/visitor/components/contact/constants/texts.js
@@ -1,4 +1,4 @@
 export const CONTACT_COMPANY = 'Warsaw Digital Sp. z o.o.'
 export const CONTACT_ADDRESS_STREET = { pl: 'ul. Twarda 18', en: 'Twarda 18 St.' }
 export const CONTACT_ADDRESS_POSTAL_CODE_AND_CITY = { pl: '00-105, Warszawa', en: '00-105, Warsaw' }
-export const CONTACT_EMAIL = { pl: 'kontakt@warsaw.digital', en: 'contact@warsaw.digital' }
+export const CONTACT_EMAIL = { pl: 'kontakt@mapawynajmu.pl', en: 'contact@mapawynajmu.pl' }
