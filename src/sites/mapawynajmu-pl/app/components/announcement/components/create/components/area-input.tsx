3fd456f8e7de0b79304b0fe550f57482a95d0b1a
tom-ash <tomasz.bogusz@outlook.com>
2022-09-25 08:27:21 +0200

    announcement/create add category tiles extended

--- src/sites/mapawynajmu-pl/app/components/announcement/components/create/components/area-input.tsx
+++ src/sites/mapawynajmu-pl/app/components/announcement/components/create/components/area-input.tsx
@@ -17,18 +17,14 @@ export function AreaInput(props: AreaInputProps) {
 
   console.log(langHandler)
 
-  const classNames = { container: 'form-input with-icon text' }
+  const classNames = { container: 'form-input text area-input' }
   const value = area
   const type = 'number'
   const children = (
-    <>
-      <div className='sqm'>
-        {/* @ts-ignore */}
-        {langHandler({ pl: <span>m<span className='sq'>2</span></span>, en: 'sqm' })}
-      </div>
+    <div className='sqm'>
       {/* @ts-ignore */}
-      <SVG name='area' />
-    </>
+      {langHandler({ pl: <>m<sup>2</sup></>, en: 'sqm' })}
+    </div>
   )
   const onChange = (area: number) => changeInputs({ area })
   const areaProps = {
