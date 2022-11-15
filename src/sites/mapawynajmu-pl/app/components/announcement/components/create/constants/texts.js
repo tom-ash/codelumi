9bc496d1887c5a7e6b20d711939222c6e7d07402
tom-ash <tomasz.bogusz@outlook.com>
2022-11-15 07:09:44 +0100

    mapawynajmu.pl > listing/create/form -> Change URL

--- src/sites/mapawynajmu-pl/app/components/announcement/components/create/constants/texts.js
+++ src/sites/mapawynajmu-pl/app/components/announcement/components/create/constants/texts.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const H1 = {
   pl: <span>Dodaj ogłoszenie wynajmu nieruchomości za darmo</span>,
-  en: <span>Add a property rent listing for free</span>,
+  en: <span>Add a real estate lease listing for free</span>,
 }
 
 export const ADDITIONAL_DATA_HEADER = {
