50d337eba3d0bca75c4cf7894c663fbb1367c58a
tom-ash <tomasz.bogusz@outlook.com>
2023-03-29 17:02:37 +0200

    skillfind.tech > header > add user links

--- src/sites/skillfind-tech/app/components/scaffold/header/header.tsx
+++ src/sites/skillfind-tech/app/components/scaffold/header/header.tsx
@@ -5,6 +5,9 @@ import styles from './styles/styles.scss'
 import { Logo } from './components/logo'
 import { LangSwitch } from '../../../../../shared/app/components/support/lang-switch/lang-switch'
 import { FloatClear } from '../../../../../shared/app/components/support/float-clear/float-clear'
+import { SignUpLink } from './components/links/components/sign-up/sign-up.link'
+import { SignInLink } from './components/links/components/sign-in/sign-in.link'
+import { SignOutButton } from './components/links/components/sign-out/sign-out'
 
 interface HeaderInterface {
   (props: {}): JSX.Element
@@ -14,13 +17,18 @@ const Header: HeaderInterface = props => {
   useStyles(styles)
 
   const { state, dispatch } = useStore()
-  const { app, control } = state
-  const { lang } = app
+  const { user } = state
+  const { authorized } = user
+
+  // console.log(user)
 
   return (
     <div id='header'>
       <Logo />
       <LangSwitch />
+      {!authorized && <SignUpLink />}
+      {!authorized && <SignInLink />}
+      {authorized && <SignOutButton />}
       <FloatClear />
     </div>
   )
