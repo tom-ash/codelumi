9229acaa9862d35bfbb6c363dffc07a8552217c7
tom-ash <tomasz.bogusz@outlook.com>
2023-04-08 06:55:49 +0200

    mapawynajmu.pl > add Filter

--- src/sites/mapawynajmu-pl/app/components/announcement/components/index/components/panel/components/category/category.tsx
+++ src/sites/mapawynajmu-pl/app/components/announcement/components/index/components/panel/components/category/category.tsx
@@ -1,30 +1,5 @@
 import React from 'react'
-import { SVG } from '../../../../../../../../../../shared/app/components/support/svg/svg'
-import { TextInput } from '../../../../../../../../../../shared/app/components/support/text-input/text-input'
 import { Tiles } from './components/tiles/tiles'
-import { useStore } from '../../../../../../../../../../shared/app/functions/store/useStore'
+import { Filter } from '../filter/filter'
 
-export const Category = () => {
-  const { state, dispatch } = useStore()
-  const { control } = state
-  const { showCategoryTiles } = control
-  const setControl = (value: any) => dispatch({ type: 'control', value })
-  const onClickCallback = () => setControl({ showCategoryTiles: true })
-
-  const children = <SVG name='list' />
-
-  const textInputProps = {
-    inputKey: 'category',
-    containerClassNames: 'text-input filter',
-    children,
-    disabled: true,
-    onClickCallback,
-  }
-
-  return (
-    <div className='category-filter'>
-      <TextInput {...textInputProps} />
-      {showCategoryTiles && <Tiles />}
-    </div>
-  )
-}
+export const Category = () => <Filter name='category' panel={<Tiles />} />
---
+++ src/sites/mapawynajmu-pl/app/components/announcement/components/index/components/panel/components/filter/filter.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useStore } from '../../../../../../../../../../shared/app/functions/store/useStore'
+import { TextInput } from '../../../../../../../../../../shared/app/components/support/text-input/text-input'
+import { SVG } from '../../../../../../../../../../shared/app/components/support/svg/svg'
+
+interface FilterInterface {
+  (props: {
+    name: string
+    panel: React.ReactElement
+  }): React.ReactElement
+}
+
+export const Filter: FilterInterface = (props) => {
+  const { name, panel } = props
+  const { state, dispatch } = useStore()
+  const { data } = state
+  const { [`${name}SvgName`]: svgName } = data
+  const children = <SVG name={svgName} />
+
+  const setControl = (value: any) => dispatch({ type: 'control', value })
+  const onClickCallback = () => setControl({ [`${name}FilterPanel`]: true })
+
+  const textInputProps = {
+    inputKey: name,
+    containerClassNames: `text-input filter ${name}`,
+    children,
+    disabled: true,
+    onClickCallback,
+  }
+
+  return (
+    <div className={`filter ${name}`}>
+      <TextInput {...textInputProps} />
+      {panel}
+    </div>
+  )
+}
