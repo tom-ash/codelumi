820a2692685fd4cc6c5b0334202de7ee09a7baf9
tom-ash <tomasz.bogusz@outlook.com>
2023-05-21 17:07:41 +0200

    shared > page > add PingedAt to IndexNow

--- src/sites/shared/app/components/page/components/edit/components/index-now/components/button/button.tsx
+++ src/sites/shared/app/components/page/components/edit/components/index-now/components/button/button.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { useStore } from '../../../../../../functions/store/useStore'
-import { indexNow as submit } from './functions/index-now'
-import { SubmitButton } from '../../../../../support/submit-button/submit-button'
+import { useStore } from '../../../../../../../../functions/store/useStore'
+import { indexNow as submit } from '../../functions/index-now'
+import { SubmitButton } from '../../../../../../../support/submit-button/submit-button'
 
-interface IndexNowButtonInterface {
+interface ButtonInterface {
   (): React.ReactElement
 }
 
-export const IndexNowButton: IndexNowButtonInterface = () => {
+export const Button: ButtonInterface = () => {
   const { state } = useStore()
   const { texts, data, inputs } = state
   const { indexNowButtonLabel } = texts
@@ -16,7 +16,6 @@ export const IndexNowButton: IndexNowButtonInterface = () => {
 
   const submitButtonProps = {
     label: indexNowButtonLabel,
-    customContainerClassNames: 'index-now',
     indexNowApiUrl,
     id,
     submit,
---
+++ src/sites/shared/app/components/page/components/edit/components/index-now/index-now.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useStore } from '../../../../../../functions/store/useStore'
+import { Button } from './components/button/button'
+
+interface IndexNowInterface {
+  (): React.ReactElement
+}
+
+export const IndexNow: IndexNowInterface = () => {
+  const { state } = useStore()
+  const { data } = state
+  const { indexNowPingedAt } = data
+
+  return (
+    <div className='index-now'>
+      <div className='pinged-at'>
+        {indexNowPingedAt}
+      </div>
+      <Button />
+    </div>
+  )
+}
