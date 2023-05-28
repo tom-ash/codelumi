7cd3213502b19aa94446621fc42a984abb997802
tom-ash <tomasz.bogusz@outlook.com>
2023-05-28 06:18:19 +0200

    mapawynajmu.pl > user > password-reset > adjust styles

--- src/sites/mapawynajmu-pl/app/components/user/components/password-reset/password-reset.tsx
+++ src/sites/mapawynajmu-pl/app/components/user/components/password-reset/password-reset.tsx
@@ -2,11 +2,25 @@ import React from 'react'
 import CommonPasswordReset from '../../../../../../shared/app/components/user/components/password-reset/password-reset'
 import useStyles from 'isomorphic-style-loader-react18/useStyles'
 import styles from './styles/styles.scss'
+import { useStore } from '../../../../../../shared/app/functions/store/useStore'
 
 const PasswordReset = () => {
   useStyles(styles)
 
-  return <CommonPasswordReset />
+  const { state } = useStore()
+  const { app } = state
+  const { minInnerHeight } = app
+
+  return (
+    <div
+      id='user-password-reset'
+      style={{
+        minHeight: minInnerHeight
+      }}
+    >
+      <CommonPasswordReset />
+    </div>
+  )
 }
 
 export default PasswordReset
