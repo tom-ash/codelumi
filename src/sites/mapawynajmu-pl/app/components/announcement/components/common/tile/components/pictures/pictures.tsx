c7b70c354c20eded28c219941d0ab1d84d3bb6fa
tom-ash <tomasz.bogusz@outlook.com>
2023-08-04 06:00:50 +0200

    mapawynajmu.pl > listing > use TypeScript for Pictures

---
+++ src/sites/mapawynajmu-pl/app/components/announcement/components/common/tile/components/pictures/pictures.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ManagedSlider from '../../../../../../support/components/managed-slider/managed-slider'
+import { SVG } from '../../../../../../../../../shared/app/components/support/svg/svg'
+import AWS_S3_URL from '../../../../../../../../shared/constants/urls/aws-s3'
+
+interface Picture {
+  database: string
+}
+
+interface ListingTilePicturesInterface {
+  (props: {
+    pictures: Picture[],
+    disableSLides: boolean,
+    title: string,
+    id: number,
+  }): React.ReactElement
+}
+
+export const ListingTilePictures:ListingTilePicturesInterface = (props) => {
+  const {
+    id,
+    title,
+    pictures,
+    disableSLides,
+  } = props
+
+  const pictureUrls = pictures.map(picture => ({
+    database: `${AWS_S3_URL}/announcements/${id}/${picture.database}`,
+  }))
+
+  const Chevron = () => <SVG name='chevron' />
+
+  return (
+    <div className='pictures'>
+      <ManagedSlider
+        disableSLides={disableSLides}
+        title={title}
+        pictures={pictureUrls}
+        chevronLeft={<Chevron />}
+        chevronRight={<Chevron />}
+      />
+    </div>
+  )
+}
+
+export default ListingTilePictures
