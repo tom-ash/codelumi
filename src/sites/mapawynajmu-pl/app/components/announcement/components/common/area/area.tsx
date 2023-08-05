e6e30a4db4d1f7dc02e9cd24db33552fec11cd97
tom-ash <tomasz.bogusz@outlook.com>
2023-08-05 09:23:55 +0200

    mapawynajmu.pl > listing > common > handle decimal in Area

--- src/sites/mapawynajmu-pl/app/components/announcement/components/common/area/area.tsx
+++ src/sites/mapawynajmu-pl/app/components/announcement/components/common/area/area.tsx
@@ -9,9 +9,14 @@ export const Area: AreaInterface = props => {
     area,
   } = props
 
+  const multipliedAmount = `${Math.ceil(area * 100)}`
+  const integerAmount = multipliedAmount.slice(0, -2)
+  const decimalAmount = multipliedAmount.slice(-2)
+  const amount = decimalAmount === '00' ? integerAmount : `${integerAmount},${decimalAmount}`
+
   return (
     <div className='area'>
-      {area} m<sup>2</sup>
+      {amount} m<sup>2</sup>
     </div>
   )
 }
