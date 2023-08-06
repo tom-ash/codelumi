fa9462da40d8a4e30ed95bf273d13dabc28119cb
tom-ash <tomasz.bogusz@outlook.com>
2023-08-06 17:03:20 +0200

    shared > update api url

--- src/sites/skillfind-tech/shared/functions/getters/api-url.js
+++ src/sites/skillfind-tech/shared/functions/getters/api-url.js
@@ -1,9 +1,9 @@
 function getApiUrl() {
   switch (APP_ENV) {
     case 'development':
-      return 'http://localhost:3001/skillfind_tech'
+      return 'http://localhost:3001/skillfind-tech'
     case 'production':
-      return 'https://warsaw-digital-server.herokuapp.com/skillfind_tech'
+      return 'https://warsaw-digital-server.herokuapp.com/skillfind-tech'
     default:
       return ''
   }
