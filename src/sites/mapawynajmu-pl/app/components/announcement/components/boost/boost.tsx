acd017e69704e234c0ec8edbac9140d1f93c9153
tom-ash <tomasz.bogusz@outlook.com>
2023-08-13 13:51:34 +0200

    mapawynajmu.pl > listing > boost > add PayU order creation

--- src/sites/mapawynajmu-pl/app/components/announcement/components/boost/boost.tsx
+++ src/sites/mapawynajmu-pl/app/components/announcement/components/boost/boost.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useStyles from 'isomorphic-style-loader-react18/useStyles'
 import styles from './styles/styles.scss'
 import { Spinner } from '../../../support/components/spinner/spinner'
+import { useStore } from '../../../../../../shared/app/functions/store/useStore'
 
 interface ListingBoostInterface {
   (): React.ReactElement
@@ -9,6 +10,15 @@ interface ListingBoostInterface {
 
 const ListingBoost: ListingBoostInterface = () => {
   useStyles(styles)
+
+  const { state: { data: { orderHref }} } = useStore()
+
+  useEffect(() => {
+    // @ts-ignore
+    window.gtag('event', 'promotion_added')
+
+    location.href = orderHref
+  }, [])
 
   return (
     <div id='listing-boost'>
