b7e5f437eced24d7747739530ba073e060c07be8
tom-ash <tomasz.bogusz@outlook.com>
2023-09-05 08:38:45 +0200

    shared > fix empty headers[accept-language]

--- src/sites/shared/server/functions/handlers/generic-request.js
+++ src/sites/shared/server/functions/handlers/generic-request.js
@@ -14,7 +14,8 @@ function genericRequestHandler({ req, res, clientUrl, apiUrl, appRenderer, siteN
   if (url === 'sitemap.xml') return sitemapSender({ res, clientUrl, apiUrl })
 
   const deviceFromHeader = headers['user-agent']
-  const langFromHeader = headers['accept-language'].split(',')[0]
+  const langsHeader = headers['accept-language']
+  const langFromHeader = langsHeader ? langsHeader.split(',')[0] : 'None' // TODO!
 
   routeSender({
     res,
