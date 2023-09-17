15e4ad08f4a41e8afd57fe699aeb585f8b2eb33a
tom-ash <tomasz.bogusz@outlook.com>
2023-09-17 12:49:03 +0200

    shared > add useApp

---
+++ src/sites/shared/app/functions/store/use-app.ts
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux"
+import { Store, StoreObject } from "../root-reducer/root-reducer"
+
+interface UseApp {
+  (): StoreObject
+}
+
+export const useApp: UseApp = () => {
+  const render = useSelector((state: Store) => state.app)
+
+  return render
+}
