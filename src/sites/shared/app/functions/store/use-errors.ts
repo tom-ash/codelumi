a33b6895e98292a56df3c0557370c0eb9391be90
tom-ash <tomasz.bogusz@outlook.com>
2023-09-17 14:39:29 +0200

    shared > add useErrors

---
+++ src/sites/shared/app/functions/store/use-errors.ts
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux"
+import { Store, StoreObject } from "../root-reducer/root-reducer"
+
+interface UseErrors {
+  (): StoreObject
+}
+
+export const useErrors: UseErrors = () => {
+  const render = useSelector((state: Store) => state.errors)
+
+  return render
+}
