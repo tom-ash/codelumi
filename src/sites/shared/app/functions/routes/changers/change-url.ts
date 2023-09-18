67a6b33e979b34703bf5128aca95ebccb44db922
tom-ash <tomasz.bogusz@outlook.com>
2023-09-18 07:41:59 +0200

    shared > add origin to protocolless href in changeUrl

--- src/sites/shared/app/functions/routes/changers/change-url.ts
+++ src/sites/shared/app/functions/routes/changers/change-url.ts
@@ -3,6 +3,12 @@ import scrollToTop from '../../screen/scrollers/to-top'
 // @ts-ignore
 export const changeUrl = ({ href, withScroll = true, retainQueryParams = false }) => {
   let decoratedHref = href.replace(/\/\/$/, '/')
+
+  if (decoratedHref.indexOf('://') === -1) {
+    decoratedHref = decoratedHref.replace(/^\//, '')
+    decoratedHref = window.location.origin + '/' + decoratedHref
+  }
+
   decoratedHref = retainQueryParams ? decoratedHref + location.search : decoratedHref
 
   history.pushState({}, '', decoratedHref)
