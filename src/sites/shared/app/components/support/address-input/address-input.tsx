1c115b520e76178f93685c80fe108c4411972951
tom-ash <tomasz.bogusz@outlook.com>
2023-10-07 07:24:16 +0200

    mapawynajmu.pl > root > add arrow traversal to location filter

--- src/sites/shared/app/components/support/address-input/address-input.tsx
+++ src/sites/shared/app/components/support/address-input/address-input.tsx
@@ -3,36 +3,52 @@ import { ManagedText } from 'managed-inputs'
 import { useInputs } from '../../../functions/store/use-inputs'
 import { useData } from '../../../functions/store/use-data'
 import { useDispatch } from 'react-redux'
-// import { getLocationByPlaceId } from '../../../functions/map/google/get-location-by-place-id'
 import { GooglePlacesAutocompleteItem } from '../../../types/google-places-autocomplete'
 
 interface AddressInputInterface {
   (props: {
+    autocompletes: GooglePlacesAutocompleteItem[],
+    currentAutocompleteItemIndex: number,
     onInputEnter?(location: GooglePlacesAutocompleteItem): void
+    setCurrentAutocompleteItem(autocompleteItem: number): void
   }): React.ReactElement
 }
 
 export const AddressInput: AddressInputInterface = (props) => {
-  const { onInputEnter } = props
+  const { currentAutocompleteItemIndex, onInputEnter, setCurrentAutocompleteItem } = props
   const { location } = useInputs()
   const { autocompletes } = useData()
+  const classNames = 'address-input'
+  const value = location || ''
+  const placeholder = 'Lokalizacja' // TODO: Use server localization.
   const dispatch = useDispatch()
   const setInputs = (value: any) => dispatch({ type: 'inputs', value })
   const setData = (value: any) => dispatch({ type: 'data', value })
   const setControl = (value: any) => dispatch({ type: 'control', value })
-  const classNames = 'address-input'
-  const value = location || ''
-  const placeholder = 'Lokalizacja'
   const onFocus = () => setControl({ showAutocompletes: true })
+  const onKeyDown = (_value: any, e: React.KeyboardEvent) => {
+    const key = e.key
+
+    if (key === 'ArrowDown') {
+      const newCurrentAutocompleteItem = currentAutocompleteItemIndex === autocompletes.length - 1 ? 0 : currentAutocompleteItemIndex + 1
+
+      setCurrentAutocompleteItem(newCurrentAutocompleteItem)
+    } else if (key === 'ArrowUp') {
+      const newCurrentAutocompleteItem = currentAutocompleteItemIndex === 0 ? autocompletes.length - 1 : currentAutocompleteItemIndex - 1
+
+      setCurrentAutocompleteItem(newCurrentAutocompleteItem)
+    }
+  }
   const onEnter = async (e: KeyboardEvent) => {
     e.preventDefault()
 
+    setCurrentAutocompleteItem(0)
+
     if (autocompletes.length === 0) return
 
-    onInputEnter && onInputEnter(autocompletes[0])
+    onInputEnter && onInputEnter(autocompletes[currentAutocompleteItemIndex])
 
-    // @ts-ignore
-    // https://stackoverflow.com/questions/60504810/react-synteticevent-provides-no-blur-method-for-keyboardeventhtmlinputelement
+    // @ts-expect-error: https://stackoverflow.com/questions/60504810/react-synteticevent-provides-no-blur-method-for-keyboardeventhtmlinputelement
     e.target.blur()
   }
   const onTab = () => {
@@ -41,9 +57,11 @@ export const AddressInput: AddressInputInterface = (props) => {
   const onChange = (input: any) => {
     setInputs({ location: input })
 
+    setCurrentAutocompleteItem(0)
+
     if (!input) return setData({ autocompletes: [] })
 
-    // @ts-ignore
+    // @ts-expect-error: TODO
     const sessionToken = window.sessionToken
     const autocompleteService = new google.maps.places.AutocompleteService()
 
@@ -67,6 +85,7 @@ export const AddressInput: AddressInputInterface = (props) => {
     placeholder,
     onFocus,
     onEnter,
+    onKeyDown,
     onTab,
     onChange,
   }
--- src/sites/shared/app/components/support/google-places-autocomplete/google-places-autocomplete.tsx
+++ src/sites/shared/app/components/support/google-places-autocomplete/google-places-autocomplete.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SVG } from '../svg/svg'
 import { AddressInput } from '../address-input/address-input'
 import { useData } from '../../../functions/store/use-data'
@@ -15,18 +15,28 @@ export const GooglePlacesAutocomplete: GooglePlacesAutocompleteInterface = (prop
   const data = useData()
   const autocompletes = data.autocompletes as GooglePlacesAutocompleteItem[]
   const { onItemClick, onInputEnter } = props
+  const [currentAutocompleteItemIndex, setCurrentAutocompleteItem] = useState(0)
 
   return (
     <div className='filter location'>
       <SVG name='marker' />
-      <AddressInput onInputEnter={onInputEnter} />
+      <AddressInput
+        onInputEnter={onInputEnter}
+        autocompletes={autocompletes}
+        currentAutocompleteItemIndex={currentAutocompleteItemIndex}
+        setCurrentAutocompleteItem={setCurrentAutocompleteItem}
+      />
       {autocompletes && autocompletes.length > 0 && (
         <div className='autocompletes'>
-          {autocompletes.map(autocomplete => {
+          {autocompletes.map((autocomplete, index) => {
             const { description } = autocomplete
+            const className = currentAutocompleteItemIndex === index ? 'current' : undefined
 
             return (
-              <div onClick={() => onItemClick && onItemClick(autocomplete)}>
+              <div
+                onClick={() => onItemClick && onItemClick(autocomplete)}
+                className={className}
+              >
                 {description}
               </div>
             )
