4ddaef7bfd99de7e7e8fd302b3ea6af207d4ef41
tom-ash <tomasz.bogusz@outlook.com>
2023-11-11 12:22:06 +0100

    shared > sync > temporarily remove appendAuthor

--- src/sites/shared/app/functions/routes/setters/meta.ts
+++ src/sites/shared/app/functions/routes/setters/meta.ts
@@ -1,29 +1,31 @@
 import getPureUrl from '../../../../shared/functions/routes/getters/pure-url'
 import { openGraphBuilder } from '../../../../shared/functions/builders/open-graph-builder'
 
-interface AppendAuthor {
-  (author: string): void
-}
+// interface AppendAuthor {
+//   (author: string): void
+// }
 
-const appendAuthor: AppendAuthor = author => {
-  const headElement = document.querySelector('head')
-  const authorMetaElement = document.querySelector('meta[name="author"]')
+// const appendAuthor: AppendAuthor = author => {
+//   // const headElement = document.querySelector('head')
+//   const authorMetaElement = document.querySelector('meta[name="author"]')
 
-  if (author) {
-    if (authorMetaElement) {
-      document.querySelector('meta[name="author"]')!.setAttribute('content', author)
-    } else {
-      const newAuthorMetaElement = document.createElement('meta')
-      newAuthorMetaElement.setAttribute('name', 'author')
-      newAuthorMetaElement.content = author
-      headElement!.appendChild(newAuthorMetaElement)
-    }
-  } else {
-    if (authorMetaElement) {
-      headElement!.removeChild(authorMetaElement)
-    }
-  }
-}
+//   if (author) {
+//     if (authorMetaElement) {
+//       document.querySelector('meta[name="author"]')!.setAttribute('content', author)
+//     } else {
+//       const headElement = document.querySelector('head')
+//       const newAuthorMetaElement = document.createElement('meta')
+//       newAuthorMetaElement.setAttribute('name', 'author')
+//       newAuthorMetaElement.content = author
+//       headElement?.appendChild(newAuthorMetaElement)
+//     }
+//   } else {
+//     if (authorMetaElement) {
+//       const headElement = document.querySelector('head')
+//       headElement?.removeChild(authorMetaElement)
+//     }
+//   }
+// }
 
 // @ts-ignore
 const replaceOpenGraph = openGraphProps => {
@@ -41,7 +43,7 @@ function metaSetter(meta) {
 
   document.documentElement.lang = lang
   document.title = title
-  appendAuthor(author)
+  // appendAuthor(author)
   document.querySelector('meta[name="description"]')!.setAttribute('content', description)
   document.querySelector('meta[name="keywords"]')!.setAttribute('content', keywords)
   document.querySelector('link[rel="canonical"]')!.setAttribute('href', getPureUrl(`${clientUrl}/${canonicalPath}`))
