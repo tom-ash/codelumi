4e4187d8c8be75b594e50025e4fd22fb09fd1494
Tomasz Bogusz <tomaszbogusz@outlook.com>
2024-02-17 08:44:28 +0100

    shared > add semanticize

--- src/sites/skillfind-tech/app/components/posting/components/create/components/form/form.tsx
+++ src/sites/skillfind-tech/app/components/posting/components/create/components/form/form.tsx
@@ -5,7 +5,7 @@ import Skills from './components/skill-selector/skills'
 import SubmitButton from './components/submit-button'
 import { Contracts } from './components/contracts/contracts'
 import { useTexts } from '../../../../../../../../shared/app/functions/store/use-texts'
-// import { TypedTextInput } from '../../../../../../../../shared/app/components/support/typed-text-input/typed-text-input'
+import { TypedTextInput } from '../../../../../../../../shared/app/components/support/typed-text-input/typed-text-input'
 
 const AnnouncementCreateForm = () => {
   useStyles(styles)
@@ -22,13 +22,13 @@ const AnnouncementCreateForm = () => {
       <section>
         <h2>COMPANY DATA</h2>
 
-        {/* <TypedTextInput
+        <TypedTextInput
           inputKey='businessName'
           onBlur={() => {
             console.log("1234132131231")
           }}
           error='asdasd'
-        /> */}
+        />
       </section>
 
       <h2>SKILLS</h2>
