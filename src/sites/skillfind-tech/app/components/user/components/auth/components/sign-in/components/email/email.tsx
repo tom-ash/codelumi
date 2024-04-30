ab28c700b36e4643177141cdee709c883f8dbca7
Tomasz Bogusz <tomaszbogusz@outlook.com>
2024-04-30 17:05:14 +0200

    feat(skillfind.tech/visitor/users) adjust auth styles

--- src/sites/skillfind-tech/app/components/user/components/auth/components/sign-in/components/email/email.tsx
+++ src/sites/skillfind-tech/app/components/user/components/auth/components/sign-in/components/email/email.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import { Line } from '../../../../../../../../../../mapawynajmu-pl/app/components/support/components/line/line'
 import { EmailAddressInput } from '../../../../../../../../../../shared/app/components/user/components/common/components/email-address/email-address.input'
 import { PasswordInput } from '../../../../../../../../../../shared/app/components/user/components/common/components/password/password.input'
 import { PasswordAutoComplete } from '../../../../../../../../../../shared/app/components/user/components/common/components/password/password.input'
 import { passwordValidator } from '../../../../../../../../../../shared/app/components/user/components/auth/components/sign-in/components/password/password.validator'
 import { SignInButton } from './components/sign-in-button'
-import { Heading } from '../../../../../../../../../../mapawynajmu-pl/app/components/support/components/heading'
 import { CredentialsError } from '../../../../../../../../../../shared/app/components/user/components/auth/components/sign-in/components/credentials-error'
 import useStyles from 'isomorphic-style-loader-react18/useStyles'
 import styles from './styles/styles.scss'
-import { changeUrl } from '../../../../../../../../../../shared/app/functions/routes/changers/change-url'
 import { useTexts } from '../../../../../../../../../../shared/app/functions/store/use-texts'
+import { Link } from '../../../../../../../../../../shared/app/components/support/link/link'
+import { MainHeading } from '../../../../../../../../../../shared/app/components/support/headings/main-heading'
 
 const UserAuthEmail = () => {
   useStyles(styles)
@@ -21,25 +20,16 @@ const UserAuthEmail = () => {
     validator: passwordValidator,
   }
 
-  const headingProps = {
-    tier: 1,
-    text: headingOne,
-  }
-
   return (
     <div id='user-auth-email'>
-      <Heading {...headingProps} />
-      <Line />
+      <MainHeading icon='signIn'/>
       <form>
         <EmailAddressInput />
         <PasswordInput {...passwordInputProps} />
         <CredentialsError />
         <SignInButton />
       </form>
-      {/* <Line />
-      <div className='links'>
-        <span onClick={() => changeUrl({ href: resetPasswordHref })}>{resetPassword}</span>
-      </div> */}
+      <Link linkKey='user/password-reset' />
     </div>
   )
 }
