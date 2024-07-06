888d5145303029b40f243a9df7335d7ed3d79336
Tomasz Bogusz <tomaszbogusz@outlook.com>
2024-07-06 06:54:58 +0200

    feat(skillfind.tech/visitor): add about

---
+++ src/sites/skillfind-tech/app/components/scaffold/footer/components/about-skillfind-tech/about-skillfind-tech.link.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from '../../../../../../../shared/app/components/support/link/link'
+
+export const AboutSkillfindTechLink = () => {
+  const linkProps = {
+    linkKey: 'visitor/about-skillfind-tech',
+    customClassNames: 'link',
+  }
+
+  return <Link {...linkProps} />
+}
---
+++ src/sites/skillfind-tech/app/components/visitor/components/about/about.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import useStyles from 'isomorphic-style-loader-react18/useStyles'
+import styles from './styles/styles.scss'
+import { useTexts } from '../../../../../../shared/app/functions/store/use-texts'
+import { MainHeading } from '../../../../../../shared/app/components/support/headings/main-heading'
+import FeatureItem from '../../../../../../shared/app/components/support/render-node/components/feature-item/feature-item'
+
+const VisitorContact = () => {
+  useStyles(styles)
+
+  const { intro, features } = useTexts()
+
+  console.log('features', features)
+
+  return (
+    <div id='about'>
+      <div className='container'>
+        <MainHeading />
+        <div className='intro'>
+          <strong>
+            {intro}
+          </strong>
+        </div>
+        <ul className='features'>
+          {features.map((feature: any) => {
+            const {
+              heading,
+              imgSrc,
+              imgAlternate,
+              text,
+            } = feature
+
+            return (
+              <li>
+                <FeatureItem
+                  heading={heading}
+                  imgSrc={imgSrc}
+                  imgAlternate={imgAlternate}
+                  text={text}
+                />
+              </li>
+            )
+          })}  
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default VisitorContact
